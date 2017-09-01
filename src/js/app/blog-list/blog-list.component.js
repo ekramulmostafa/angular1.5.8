73c6b180666f599f8bdcb3026c8cae83591caaf7
ekramulmostafa <ekramul.mostafa@gmail.com>
2017-09-01 20:25:42 +0600

    component making

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -1,14 +1,20 @@
 'use stric';
 
-angular.module('blogList').controller('BlogListController', function($scope){
-    console.log('hello controller');
-    $scope.title = 'Hi there';
-    $scope.click = 0;
+angular.module('blogList').component('blogList', {
+    template: "<div class=\"\">\n" +
+    "    {{ title }}\n" +
+    "    <button ng-click=\"clickMe()\">Click here</button>\n" +
+    "</div>",
+    controller: function ($scope) {
+        console.log('hello controller');
+        $scope.title = 'Hi there';
+        $scope.click = 0;
 
-    $scope.clickMe = function(){
-        console.log('clicked');
-        $scope.click += 1;
-        $scope.title = 'changed title = '+$scope.click;
+        $scope.clickMe = function(){
+            console.log('clicked');
+            $scope.click += 1;
+            $scope.title = 'changed title = '+$scope.click;
 
+        }
     }
 });
