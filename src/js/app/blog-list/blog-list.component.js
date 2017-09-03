a67859747651ae82494ad4c2881292ef0ae9293a
ekramulmostafa <ekramul.mostafa@gmail.com>
2017-09-03 19:57:52 +0600

    Looping data

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -3,7 +3,27 @@
 angular.module('blogList').component('blogList', {
     templateUrl:'./template/blog-list.html',
     controller: function ($scope) {
+
+        var blogItems = [
+            {
+                title: "some title",
+                id: 1,
+                description: "this is a book"
+            },
+            {
+                title: "tea title",
+                id: 2,
+                description: "this is a book 2"
+            },
+            {
+                title: "title 3",
+                id: 3,
+                description: "this is a book 3"
+            }]
+
         console.log('hello controller');
+
+        $scope.blogList = blogItems;
         $scope.title = 'Hi there';
         $scope.click = 0;
 
